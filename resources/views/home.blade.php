8cf19b0341fd1047e71def3a4c330b32be62cf90
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2024-03-21 14:55:09 +0300

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -83,7 +83,22 @@ https://templatemo.com/tm-558-klassy-cafe
                             </li>
                             <!-- <li class=""><a rel="sponsored" href="https://templatemo.com" target="_blank">External URL</a></li> -->
                             <li class="scroll-to-section"><a href="#reservation">Contact Us</a></li> 
-                        </ul>        
+                            <li> 
+                          @if (Route::has('login'))
+                              <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+                                @auth
+                                 <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                                  @else
+                                     <li><a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a></li>
+                                      @if (Route::has('register'))
+                                        <li><a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a></li>
+                                     @endif
+                                  @endauth
+                               </div>
+                         @endif
+                            </li>
+                        </ul>       
+                         
                         <a class='menu-trigger'>
                             <span>Menu</span>
                         </a>
