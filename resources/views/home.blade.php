2527526336d9e4b6c6cddef47ca13dd6d2390ad6
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2024-03-21 22:34:24 +0300

    updates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -87,7 +87,9 @@ https://templatemo.com/tm-558-klassy-cafe
                           @if (Route::has('login'))
                               <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
                                 @auth
-                                 <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                                      <x-app-layout>
+    
+                                      </x-app-layout>
                                   @else
                                      <li><a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a></li>
                                       @if (Route::has('register'))
