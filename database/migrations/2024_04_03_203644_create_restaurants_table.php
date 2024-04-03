b1b1849bad9c849d2240f0f336bca0598b8639ba
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2024-04-03 20:44:09 +0300

    update

---
+++ database/migrations/2024_04_03_203644_create_restaurants_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('category_id'); 
+            $table->foreign('category_id')->references('id')->on('category')->onDelete('cascade');
+            $table->string('price_range');
+            $table->string('location');
+            $table->float('rating')->nullable();
+            $table->text('description')->nullable();
+            $table->string('image_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('restaurants');
+    }
+};
