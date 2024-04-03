3a16de08af27bc19fb2c7f42b94a729aa9aa0dcf
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2024-04-03 20:54:30 +0300

    Create 2024_04_03_204536_create_dishes_table.php

---
+++ database/migrations/2024_04_03_204536_create_dishes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dishes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('restaurant_id'); 
+            $table->foreign('restaurant_id')->references('id')->on('restaurants')->onDelete('cascade'); // Corrected foreign key constraint
+            $table->text('description')->nullable();
+            $table->decimal('price', 8, 2)->nullable();
+            $table->string('image_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dishes');
+    }
+};
