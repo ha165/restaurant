9a78dd01d1877f1889f4049b3a9ad1c41c0fa64f
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2024-04-03 10:28:52 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,8 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/home', ([HomeController::class, 'index']));
+Route::get('/', ([HomeController::class, 'index']));
 
 Route::middleware([
     'auth:sanctum',
