52a11c378cf4e6b974ad7f6d16fdbde8c2a16864
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2024-04-03 10:56:33 +0300

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Admin Page</title>
-</head>
-<body>
-    This is Admin Page
-</body>
-</html>
+<x-app-layout>
+    
+</x-app-layout>
