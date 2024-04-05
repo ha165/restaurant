3826725ccc941778a764146e86fe97ce00a14d16
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2024-04-05 12:33:32 +0300

    updates

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -10,7 +10,7 @@
                     @foreach($restaurants as $restaurant)
                         <div class="col-lg-4 mb-4">
                             <div class="card">
-                                <img src="{{ asset($restaurant->image_url) }}" class="card-img-top" alt="Restaurant Image">
+                               <img src="{{ asset('storage/' . $restaurant->image_url) }}" class="card-img-top" alt="Restaurant Image">
                                 <div class="card-body">
                                     <h5 class="card-title">{{ $restaurant->name }}</h5>
                                     <p class="card-text">Price Range: {{ $restaurant->price_range }}</p>
@@ -25,10 +25,11 @@
             @endif
             </div>
             <div class="col-lg-4">
-                <h2>You May Also Like</h2>
+                <h2>Matching Results</h2>
                 @foreach($closestMatches as $match)
                     <div class="card mb-4">
                         <div class="card-body">
+                           <img src="{{ asset('storage/' . $match->image_url) }}" class="card-img-top" alt="Restaurant Image">
                             <h5 class="card-title">{{ $match->name }}</h5>
                             <p class="card-text">Price Range: {{ $match->price_range }}</p>
                             <p class="card-text">Location: {{ $match->location }}</p>
